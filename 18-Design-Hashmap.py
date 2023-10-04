6e4a607ae81e49a8304a72071767feaf5595a5c5
Saurav Pandey <84031432+sp4398@users.noreply.github.com>
2023-10-04 23:28:29 +0530

    Create 18-Design-Hashmap.py

---
+++ 18-Design-Hashmap.py
@@ -0,0 +1,20 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.data=[None]*1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.data[key]=value
+
+    def get(self, key: int) -> int:
+        value=self.data[key]
+        return value if value !=None else -1
+    def remove(self, key: int) -> None:
+        self.data[key]=None
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
